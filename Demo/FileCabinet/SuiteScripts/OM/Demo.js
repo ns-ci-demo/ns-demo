c6798b77a3eb40e049bf96a8c3eb3b8f0337259a
ns-ci-demo <secret30>
2017-06-23 21:26:33 +0530

    Initial version

---
+++ Demo/FileCabinet/SuiteScripts/OM/Demo.js
@@ -0,0 +1,10 @@
+
+function UpdateAfterSubmit(type, form)
+{
+    // load sales order record
+
+var salesorder = nlapiLoadRecord("salesorder", 304);
+salesorder.setFieldValue('custbody_demo_so_field', 'Demo - US');
+nlapiSubmitRecord(salesorder);
+    
+}
