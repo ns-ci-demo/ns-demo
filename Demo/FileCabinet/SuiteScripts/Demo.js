6d44a7779d01916e358e6d596af070acdb6fc8b2
maria.auxilia <maria.auxilia@BDC11-L-7200GP1.dir.svc.accenture.com>
2017-11-06 17:44:13 +0530

    Added comment

--- Demo/FileCabinet/SuiteScripts/Demo.js
+++ Demo/FileCabinet/SuiteScripts/Demo.js
@@ -1,7 +1,8 @@
-/*Version 1.0*/
+/*Version 2.0*/
 function UpdateAfterSubmit(type, form)
 {
     // load sales order record
+	//Modified by maria.auxilia
 
 var salesorder = nlapiLoadRecord("salesorder", 304);
 salesorder.setFieldValue('custbody_demo_so_field', 'Demo - US');
